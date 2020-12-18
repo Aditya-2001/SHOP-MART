a2342f0752e938a9d860aa0272c2f8065158ff4c
Aditya-2001 <iit2019210@iiita.ac.in>
2020-12-19 03:45:53 +0530

    signup

--- shop/home/urls.py
+++ shop/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,signup_page,emailverification,otpverification
+from .views import home,signup_page,emailverification,otpverification,signupdetails
 
 urlpatterns = [
     path('',home,name='home'),
     path('signup_page/',signup_page,name="signup_page"),
     path('emailverification/',emailverification,name="emailverification"),
     path('otpverification/',otpverification,name="otpverification"),
+    path('signupdetails/',signupdetails,name="signupdetails"),
 ]
--- shop/home/views.py
+++ shop/home/views.py
@@ -7,6 +7,7 @@ from django.core.mail import send_mail
 import math,random
 from .models import OTP
 import datetime
+from .forms import UserForm
 # Create your views here.
 def home(request):
     return render(request,"home/home.html",context={})
@@ -49,6 +50,7 @@ def otpverification(request):
     if request.method=="POST":
         email=request.POST.get('email')
         otp=str(request.POST.get('otp'))
+        print(email)
         try:
             user=OTP.objects.get(email=email)
             if str(user.otp)==otp:
@@ -65,13 +67,44 @@ def otpverification(request):
                     user.otp=otp=otp
                     user.date=datetime.datetime.now()
                     user.save()
+                    print('1111111111111111111111111111111111111111')
                     return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": True, "signup_details": False, "email": email, "time_exceeded": True})
                 else:
+                    print('222222222222222222222222222')
                     return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": False, "signup_details": True, "email": email})
             else:
+                print('22233333333333333333333333333333333333333332')
                 return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": True, "signup_details": False, "email": email, "wrong_otp": True})
         except:
+            print('224444444444444444444444444444444444444442')
             return render(request,"home/signup_page.html",context={"email_verification": True, "otp_verification": False, "signup_details": False, "email": email, "email_not_found": True})
+        
+    else:
+        return redirect('signup_page')
+
+def signupdetails(request):
+    if request.method=="POST":
+        form = UserForm(request.POST)
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")   
+        try:
+            checker = User.objects.get(email=email)
+            return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": False, "signup_details": True, "prev": True, "email": email, "email_matched": True, "prev_username": username, "prev_first_name": first_name, "prev_last_name": last_name})
+        except:
+            try:
+                checker = User.objects.get(username=username)
+                return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": False, "signup_details": True, "prev": True, "email": email, "username_matched": True,"prev_username": "", "prev_first_name": first_name, "prev_last_name": last_name})
+            except:
+                if form.is_valid():
+                    password = request.POST.get("password2")
+                    user = User.objects.create(username=username, email=email, first_name=first_name, last_name=last_name)
+                    user.set_password(password)
+                    user.save()
+                    return redirect('home')
+                else:
+                    return render(request,"home/signup_page.html",context={"email_verification": False, "otp_verification": False, "signup_details": True, "email": email, "password_error": True, "error": form.errors})
         
     else:
         return redirect('signup_page')
